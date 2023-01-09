cae15e284e8a2f3314280db1caec4e86dd21a1db
Aniekeme Umoren <aniekemeumoren@gmail.com>
2023-01-09 21:57:30 +0000

    added a function that returns the list of available attributes and methods of an object

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 9th 2023
+@author: Aniekeme Umoren
+"""
+
+
+def lookup(obj):
+    """
+    Returns all objects in an objects dictionary as a list
+    """
+    return dir(obj)
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,1 @@
+This folder contains codes related to inheritance in Python
