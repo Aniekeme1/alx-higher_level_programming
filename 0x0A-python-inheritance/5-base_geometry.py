69038de6edb02e3dc20af7ae838f6c1e74c5eed0
Aniekeme Umoren <aniekemeumoren@gmail.com>
2023-01-09 22:18:51 +0000

    added a function to write an empty class

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on MOn 9th Jan 2023
+@author: Aniekeme Umoren
+"""
+
+
+class BaseGeometry():
+    """
+    An empty class
+    """
+    pass
