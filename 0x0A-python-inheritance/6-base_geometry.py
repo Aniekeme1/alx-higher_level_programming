5fd560a93e39c9a600b51a84303ca2fc8753da03
Aniekeme Umoren <aniekemeumoren@gmail.com>
2023-01-09 22:22:34 +0000

    added a class based on the previous task

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Jan 9th 2023
+@author: Aniekeme Umoren
+"""
+
+
+class BaseGeometry():
+    """
+    An empty class
+    """
+    pass
+
+    def area(self):
+        """
+        Public instance method that calculates the area
+
+        Raises:
+            Exception if area is not imlemented
+        """
+        raise Exception("area() is not implemented")
