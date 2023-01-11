bd227221bf0b92556df901a3fdedb60285e4c2bb
Aniekeme Umoren <aniekemeumoren@gmail.com>
2023-01-11 00:22:23 +0000

    added a class to define a student by some parameters

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed 11th Jan 2023
+@author: Aniekeme Umoren
+"""
+
+
+class Student:
+    """
+    Student class
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        init method for Student class
+        Attributes:
+            first_name (str): The first name of the student
+            last_name (str): The last name of the student
+            age (int): The age of the student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Represents of Student into json format
+        Return:
+            Student class as a json format
+        """
+        return {key: value for (key, value) in self.__dict__.items()
+                if key in list(self.__dict__.keys())}
