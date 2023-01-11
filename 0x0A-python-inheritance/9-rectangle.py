062dcfd33308d0fff838e5bf7515dbfd5c074874
Aniekeme Umoren <aniekemeumoren@gmail.com>
2023-01-11 01:50:45 +0000

    added a class rectangle that inherits from the previous task class

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3|
+# -*- coding: utf-8 -*-
+"""
+Created on Wed 11th jan 2023
+@author: Aniekeme Umoren
+"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    A Rectangle class shape, inheirts from BaseGeometry
+    """
+    def __init__(self, width, height):
+        """
+        init function for Rectangle
+        Attributes:
+            width (int): the width of the rectangle
+            height (int): the height of the rectangle
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def __str__(self):
+        """
+        str funtion for rectangle
+        Returns:
+            Return width/height
+        """
+        return '[Rectangle] ' + str(self.__width) + '/' + str(self.__height)
+
+    def area(self):
+        """
+        Calculates the area of the rectangle
+        Return:
+           The area of the rectangle
+        """
+        return self.__width * self.__height
