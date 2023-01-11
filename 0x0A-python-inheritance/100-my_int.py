58b3bc350c2a6b51e778a3abb02e8a1dae698048
Aniekeme Umoren <aniekemeumoren@gmail.com>
2023-01-11 02:02:37 +0000

    added a class that inherits from an int

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed 11th Jan 2023
+@author: Aniekeme Umoren
+"""
+
+
+class MyInt(int):
+    """
+    A class that inherits from int
+    """
+    def __eq__(self, num):
+        """
+        equal function for MyInt class
+        Attributes:
+            num (int): an inputed integer
+        Returns:
+            The opositive booleanof the input
+        """
+        return (int(self) != num)
+
+    def __ne__(self, num):
+        """
+        no equal function for MyInt class
+        Attributes:
+            num (int): an inputed integer
+        Returns:
+            The opositive booleanof the input
+        """
+        return (int(self) == num)
