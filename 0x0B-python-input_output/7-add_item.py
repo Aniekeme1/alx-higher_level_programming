c1bb697523ff7e1f35f7dc44e8a837d66f4a1552
Aniekeme Umoren <aniekemeumoren@gmail.com>
2023-01-11 00:07:22 +0000

    work please

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed 11th Jan 2023
+@author: Aniekeme Umoren
+"""
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+try:
+    new_list = load_from_json_file('add_item.json')
+except:
+    new_list = []
+
+for i in range(1, len(sys.argv)):
+    new_list.append(sys.argv[i])
+
+try:
+    save_to_json_file(new_list, 'add_item.json')
+except:
+    pass
