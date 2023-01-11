095097ff4e77d6a5041626bf42086c3655a33603
Aniekeme Umoren <aniekemeumoren@gmail.com>
2023-01-11 00:16:11 +0000

    added a function that returns the dictionary description with simple data structure for json serialisation of the object

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed 11th jan 2023
+@author: Aniekeme Umoren
+"""
+
+
+def class_to_json(obj):
+    """
+    Creates a json representation of an object
+    Arguments:
+        obj (obj): The object inputted to create a class
+    Return:
+        A jason representation
+    """
+    return {key: value for (key, value) in obj.__dict__.items()
+            if key in list(obj.__dict__.keys())}
