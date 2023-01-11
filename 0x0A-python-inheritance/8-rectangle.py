60b08e030db136573285f074dec4113c6a5f0866
Aniekeme Umoren <aniekemeumoren@gmail.com>
2023-01-11 01:48:12 +0000

    added a class that inherits from previous task class

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed 11th Jan 2023
+@author: Aniekeme Umoren
+"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    A Rectangle class shape, inheirts from BaseGeometry
+    """
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
