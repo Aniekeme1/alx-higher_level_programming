1ab233b8d2ee7e65603cc5482f4518357129f907
Aniekeme Umoren <aniekemeumoren@gmail.com>
2023-04-14 03:32:21 +0000

    added a scri tthat takes in a url sends a request to the url and displays the body of the response decoded in utf 8

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,13 @@
+import urllib.request
+import urllib.error
+import sys
+
+url = sys.argv[1]
+
+try:
+    with urllib.request.urlopen(url) as response:
+        html = response.read().decode('utf-8')
+        print(html)
+except urllib.error.HTTPError as e:
+    print(f'Error code: {e.code}')
+
