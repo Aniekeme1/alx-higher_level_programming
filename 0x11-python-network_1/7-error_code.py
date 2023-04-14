393a72e486a3f175e7c5a75ada688c17c8a9454f
Aniekeme Umoren <aniekemeumoren@gmail.com>
2023-04-14 03:48:04 +0000

    added a script that takes in a url, sends a request to the url and displays the body of the response

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,11 @@
+import requests
+import sys
+
+url = sys.argv[1]
+
+response = requests.get(url)
+
+if response.status_code >= 400:
+    print("Error code: {}".format(response.status_code))
+else:
+    print(response.text)
